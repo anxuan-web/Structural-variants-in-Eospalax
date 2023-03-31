570aa2ae336f9016277e58e20ed16923a7550d99
anxuan-web <77531332+anxuan-web@users.noreply.github.com>
2023-03-31 20:15:02 +0800

    Update SV_calling.sh

--- SV_calling.sh
+++ SV_calling.sh
@@ -31,5 +31,19 @@ delly merge -o all.sites.bcf $name1.bcf $name2.bcf $name3.bcf ...
 delly call -g $reference -v all.sites.bcf -o $name1.geno.bcf $name1.sort.markdup.bam
 for file in *geno.bcf; do bcftools view $file -O v > $file.vcf; done
 #Lumpy v0.2.13
-smoove call --outdir results-smoove/ --name $sample --fasta $reference -p 1 --genotype $name1.sort.markdup.bam
-
+smoove call --outdir results-smoove/ --name $sample --fasta $reference.fa -p 1 --genotype $name1.sort.markdup.bam
+smoove merge --name merged -fasta $reference.fa ./results-smoove/*.genotyped.vcf.gz
+smoove genotype -d -x -p 1 --name $name1-joint --outdir results-genotyped/ --fasta $reference.fa --vcf merged.sites.vcf.gz $name1.sort.markdup.bam
+#Manta v1.6.0
+./manta-1.6.0.centos6_x86_64/bin/configManta.py --bam $name1.sort.markdup.bam --referenceFasta $reference.fa --runDir sample1
+python ./sample1/runWorkflow.py
+#svimmer merge three software vcf
+ls ./delly/*.vcf ./lumpy/*.vcf ./manta/*.vcf > all.vcf.list
+svimmer --threads 10 all.vcf.list LG01 LG02 LG03 LG04 ......
+./graphtyper genotype_sv $reference.fa merge.vcf.gz --sams=bam.list --region=LG01:start-end
+#gatk combined all chromosomes vcf
+gatk MergeVcfs -I concat-a.vcf -I concat-b.vcf -I concat-c.vcf ... -O combine_gatk.vcf
+#filter "BREAKPOINT" SVs
+bcftools view --include "SVMODEL='AGGREGATED'" -Oz -o get_agg.vcf.gz combine_gatk.vcf
+#filter out high degree missing SVs
+vcftools --vcf get_agg.vcf --max-missing 0.90 --recode --recode-INFO-all --out vcftools-filliter
