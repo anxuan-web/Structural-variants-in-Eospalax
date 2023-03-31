70e828c99c1a1d240fa242d278211239b737ebcc
anxuan-web <77531332+anxuan-web@users.noreply.github.com>
2023-03-31 22:05:44 +0800

    Update Large_inversion.sh

--- Large_inversion.sh
+++ Large_inversion.sh
@@ -3,4 +3,14 @@
 nucmer -t 20 --mum Efon.fa Eba.fa -p out
 delta-filter -i 90 -l 30000 -q out.delta > filter.out.delta
 show-coords -c -r filter.out.delta > filter.out.delta.coords
-#
+#JCVI v1.2.7 was used to visualize genome alignment
+python3 -m jcvi.graphics.karyotype seqids layout
+https://github.com/tanghaibao/jcvi/wiki/MCscan-(Python-version)
+#SyRI v1.5.6 was used to identify large inversions
+nucmer --maxmatch -c 100 -b 500 -l 50 refgenome qrygenome       
+delta-filter -m -i 90 -l 100 out.delta > out.filtered.delta     
+show-coords -THrd out.filtered.delta > out.filtered.coords      
+python3 $PATH_TO_SYRI -c out.filtered.coords -d out.filtered.delta -r refgenome -q qrygenome
+python3 $PATH_TO_PLOTSR syri.out refgenome qrygenome -H 8 -W 5
+https://schneebergerlab.github.io/syri/pipeline.html
+#validating the large inversions using contig-level genome of E. baileyi
