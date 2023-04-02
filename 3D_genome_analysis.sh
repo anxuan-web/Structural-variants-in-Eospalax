41d9c090abf6584241d1b1e5d5892d0a6b90e73a
anxuan-web <77531332+anxuan-web@users.noreply.github.com>
2023-04-02 11:33:48 +0800

    Update 3D_genome_analysis.sh

--- 3D_genome_analysis.sh
+++ 3D_genome_analysis.sh
@@ -15,8 +15,15 @@ done
 https://github.com/YinYuan-001/muntjac_code/blob/main/Hi-C_analysis/01.bins_mapping/work.sh
 
 #Identifying compartment type using cworld-dekker v1.0
-python runchangematrix.insulation.py -i hjf_150000_iced_hjf_chr1_dense.matrix -g hjf -c chr1 -o hjftohjf -s 150000
-perl matrix2compartment.pl -i hjf_150000_iced_hjf_chr1_dense.matrix.insulation.matrix -o 1 --et
+python runchangematrix.insulation.py -i hjf_150000_iced_hjf_LG01_dense.matrix -g hjf -c chr1 -o hjftohjf -s 150000
+perl matrix2compartment.pl -i hjf_150000_iced_hjf_LG01_dense.matrix.insulation.matrix -o 1 --et
 python matrix2EigenVectors.py -i 4.zScore.matrix.gz -r EVM.out.cor.gff3.bed -v
 
 #Identifying TAD boundary using cworld-dekker v1.0
+python runchangematrix.insulation.py -i hjf_40000_iced_hjf_LG01_dense.matrix -g hjf -c LG01 -o hjftohjf -s 40000
+perl -I /project/software/cworld-dekker-master/lib/ matrix2insulation.pl -i hjm_LG01_40000.insulation.matrix --is 1000000 --ids 240000
+
+#Identifying significant interactions by fithic v1.13
+python hicpro2fithic.py -i hjf_20000.matrix -b hjf_20000_abs.bed -s hjf_20000_iced.matrix.biases
+fithic -f fithic.fragmentMappability.gz -i fithic.interactionCounts.gz -t fithic.biases.gz -o fithic -l hjf -v -x All -r 20000
+python runfilterpvalue.qvalue.count.py hjf.spline_pass1.res20000.significances.txt.gz hjf.spline_pass1.res20000.significances.txt.bed
